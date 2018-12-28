5096858ccfdb67ab6b756a598917a586eceb768d
Shawn Rivers <kaushou@gmail.com>
2018-12-28 18:09:36 +0900

    Finish albums raw data

--- src/raw/albums.ts
+++ src/raw/albums.ts
@@ -1,10 +1,10 @@
 import { SongType } from "../common/types";
 import { IAlbum } from "../types/album";
 
-const album1: IAlbum = {
-  title: "title",
-  number: 1,
-  release: "yyyy-mm-dd",
+const bokudakenokimi: IAlbum = {
+  title: "僕だけの君〜Under Super Best〜",
+  number: 4,
+  release: "2018-01-10",
   shopping: [
     {
       title: "iTunes",
@@ -14,23 +14,167 @@ const album1: IAlbum = {
   songs: [
     {
       number: 1,
-      title: "title",
-      type: SongType.Title,
+      title: "左胸の勇気",
+      type: SongType.Under,
       focusPerformers: undefined
     },
     {
       number: 2,
-      title: "title",
-      type: SongType.Title,
+      title: "狼に口笛を",
+      type: SongType.Under,
+      focusPerformers: undefined
+    },
+    {
+      number: 3,
+      title: "涙がまだ悲しみだった頃",
+      type: SongType.Under,
+      focusPerformers: undefined
+    },
+    {
+      number: 4,
+      title: "春のメロディー",
+      type: SongType.Under,
+      focusPerformers: undefined
+    },
+    {
+      number: 5,
+      title: "13日の金曜日",
+      type: SongType.Under,
+      focusPerformers: undefined
+    },
+    {
+      number: 6,
+      title: "扇風機",
+      type: SongType.Under,
+      focusPerformers: undefined
+    },
+    {
+      number: 7,
+      title: "初恋の人を今でも",
+      type: SongType.Under,
+      focusPerformers: undefined
+    },
+    {
+      number: 8,
+      title: "生まれたままで",
+      type: SongType.Under,
+      focusPerformers: undefined
+    },
+    {
+      number: 9,
+      title: "ここにいる理由",
+      type: SongType.Under,
+      focusPerformers: undefined
+    },
+    {
+      number: 10,
+      title: "あの日 僕は咄嗟に嘘をついた",
+      type: SongType.Under,
+      focusPerformers: undefined
+    },
+    {
+      number: 11,
+      title: "君は僕と会わない方がよかったのかな",
+      type: SongType.Under,
+      focusPerformers: undefined
+    },
+    {
+      number: 12,
+      title: "別れ際、もっと好きになる",
+      type: SongType.Under,
+      focusPerformers: undefined
+    },
+    {
+      number: 13,
+      title: "嫉妬の権利",
+      type: SongType.Under,
+      focusPerformers: undefined
+    },
+    {
+      number: 14,
+      title: "不等号",
+      type: SongType.Under,
+      focusPerformers: undefined
+    },
+    {
+      number: 15,
+      title: "シークレットグラフィティー",
+      type: SongType.Under,
+      focusPerformers: undefined
+    },
+    {
+      number: 16,
+      title: "ブランコ",
+      type: SongType.Under,
+      focusPerformers: undefined
+    },
+    {
+      number: 17,
+      title: "風船は生きている",
+      type: SongType.Under,
+      focusPerformers: undefined
+    },
+    {
+      number: 18,
+      title: "アンダー",
+      type: SongType.Under,
+      focusPerformers: undefined
+    },
+    {
+      number: 19,
+      title: "My rule",
+      type: SongType.Under,
+      focusPerformers: undefined
+    },
+    {
+      number: 20,
+      title: "自由の彼方",
+      type: SongType.Under,
+      focusPerformers: undefined
+    },
+    {
+      number: 21,
+      title: "欲望のリインカーネーション",
+      type: SongType.Under,
+      focusPerformers: undefined
+    },
+    {
+      number: 22,
+      title: "君が扇いでくれた",
+      type: SongType.Under,
+      focusPerformers: undefined
+    },
+    {
+      number: 23,
+      title: "自分のこと",
+      type: SongType.Solo,
+      focusPerformers: undefined
+    },
+    {
+      number: 24,
+      title: "自惚れビーチ",
+      type: SongType.Under,
+      focusPerformers: undefined
+    },
+    {
+      number: 25,
+      title: "その女",
+      type: SongType.Under,
+      focusPerformers: undefined
+    },
+    {
+      number: 26,
+      title: "誰よりそばにいたい",
+      type: SongType.Under,
       focusPerformers: undefined
     }
   ]
 };
 
-const album2: IAlbum = {
-  title: "title",
-  number: 1,
-  release: "yyyy-mm-dd",
+const umaretekara: IAlbum = {
+  title: "生まれてから初めて見た夢",
+  number: 3,
+  release: "2017-05-24",
   shopping: [
     {
       title: "iTunes",
@@ -40,20 +184,584 @@ const album2: IAlbum = {
   songs: [
     {
       number: 1,
-      title: "title",
+      title: "裸足でSummer",
       type: SongType.Title,
       focusPerformers: undefined
     },
     {
       number: 2,
-      title: "title",
+      title: "サヨナラの意味",
       type: SongType.Title,
+      focusPerformers: undefined
+    },
+    {
+      number: 3,
+      title: "インフルエンサー",
+      type: SongType.Title,
+      focusPerformers: undefined
+    },
+    {
+      number: 4,
+      title: "シークレットグラフィティー",
+      type: SongType.Under,
+      focusPerformers: undefined
+    },
+    {
+      number: 5,
+      title: "ブランコ",
+      type: SongType.Under,
+      focusPerformers: undefined
+    },
+    {
+      number: 6,
+      title: "風船は生きている",
+      type: SongType.Under,
+      focusPerformers: undefined
+    },
+    {
+      number: 7,
+      title: "スカイダイビング",
+      type: SongType.Coupling,
+      focusPerformers: undefined
+    },
+    {
+      number: 8,
+      title: "三番目の風",
+      type: SongType.ThirdGeneration,
+      focusPerformers: undefined
+    },
+    {
+      number: 9,
+      title: "Rewindあの日",
+      type: SongType.Unit,
+      focusPerformers: undefined
+    },
+    {
+      number: 10,
+      title: "ごめんね、スムージー",
+      type: SongType.Unit,
+      focusPerformers: undefined
+    },
+    {
+      number: 11,
+      title: "醜い私",
+      type: SongType.Unit,
+      focusPerformers: undefined
+    },
+    {
+      number: 12,
+      title: "オフショアガール",
+      type: SongType.Solo,
+      focusPerformers: undefined
+    },
+    {
+      number: 13,
+      title: "君に贈る花がない",
+      type: SongType.Unit,
+      focusPerformers: undefined
+    },
+    {
+      number: 14,
+      title: "白米様",
+      type: SongType.Unit,
+      focusPerformers: undefined
+    },
+    {
+      number: 15,
+      title: "硬い殻のように抱きしめたい",
+      type: SongType.Solo,
+      focusPerformers: undefined
+    },
+    {
+      number: 16,
+      title: "満月が消えた",
+      type: SongType.Unit,
+      focusPerformers: undefined
+    },
+    {
+      number: 17,
+      title: "ワタボコリ",
+      type: SongType.Unit,
+      focusPerformers: undefined
+    },
+    {
+      number: 18,
+      title: "ないものねだり",
+      type: SongType.Solo,
+      focusPerformers: undefined
+    },
+    {
+      number: 19,
+      title: "Another Ghost",
+      type: SongType.Unit,
+      focusPerformers: undefined
+    },
+    {
+      number: 20,
+      title: "あの教室",
+      type: SongType.Unit,
+      focusPerformers: undefined
+    },
+    {
+      number: 21,
+      title: "流星ディスコティック",
+      type: SongType.Unit,
+      focusPerformers: undefined
+    },
+    {
+      number: 22,
+      title: "忘却と美学",
+      type: SongType.Unit,
+      focusPerformers: undefined
+    },
+    {
+      number: 23,
+      title: "2度目のキスから",
+      type: SongType.Unit,
+      focusPerformers: undefined
+    },
+    {
+      number: 24,
+      title: "命の真実 ミュージカル「林檎売りとカメムシ」",
+      type: SongType.Solo,
+      focusPerformers: undefined
+    },
+    {
+      number: 25,
+      title: "行くあてのない僕たち",
+      type: SongType.Unit,
+      focusPerformers: undefined
+    },
+    {
+      number: 26,
+      title: "当たり障りのない話",
+      type: SongType.Unit,
+      focusPerformers: undefined
+    }
+  ]
+};
+
+const sorezorenoisu: IAlbum = {
+  title: "それぞれの椅子",
+  number: 2,
+  release: "2016-05-25",
+  shopping: [
+    {
+      title: "iTunes",
+      url: "url"
+    }
+  ],
+  songs: [
+    {
+      number: 1,
+      title: "命は美しい",
+      type: SongType.Title,
+      focusPerformers: undefined
+    },
+    {
+      number: 2,
+      title: "太陽ノック",
+      type: SongType.Title,
+      focusPerformers: undefined
+    },
+    {
+      number: 3,
+      title: "今、話したい誰かがいる",
+      type: SongType.Title,
+      focusPerformers: undefined
+    },
+    {
+      number: 4,
+      title: "きっかけ",
+      type: SongType.Coupling,
+      focusPerformers: undefined
+    },
+    {
+      number: 5,
+      title: "太陽に口説かれて",
+      type: SongType.Coupling,
+      focusPerformers: undefined
+    },
+    {
+      number: 6,
+      title: "欲望のリインカーネーション",
+      type: SongType.Under,
+      focusPerformers: undefined
+    },
+    {
+      number: 7,
+      title: "悲しみの忘れ方",
+      type: SongType.Coupling,
+      focusPerformers: undefined
+    },
+    {
+      number: 8,
+      title: "空気感",
+      type: SongType.Unit,
+      focusPerformers: undefined
+    },
+    {
+      number: 9,
+      title: "光合成希望",
+      type: SongType.Solo,
+      focusPerformers: undefined
+    },
+    {
+      number: 10,
+      title: "無表情",
+      type: SongType.Unit,
+      focusPerformers: undefined
+    },
+    {
+      number: 11,
+      title: "あらかじめ語られるロマンス",
+      type: SongType.Unit,
+      focusPerformers: undefined
+    },
+    {
+      number: 12,
+      title: "隙間",
+      type: SongType.Unit,
+      focusPerformers: undefined
+    },
+    {
+      number: 13,
+      title: "急斜面",
+      type: SongType.Unit,
+      focusPerformers: undefined
+    },
+    {
+      number: 14,
+      title: "羽根の記憶",
+      type: SongType.Coupling,
+      focusPerformers: undefined
+    },
+    {
+      number: 15,
+      title: "乃木坂の詩",
+      type: SongType.Coupling,
+      focusPerformers: undefined
+    },
+    {
+      number: 16,
+      title: "Threefold choice",
+      type: SongType.Unit,
+      focusPerformers: undefined
+    },
+    {
+      number: 17,
+      title: "低体温のキス",
+      type: SongType.Solo,
+      focusPerformers: undefined
+    },
+    {
+      number: 18,
+      title: "遥かなるブータン",
+      type: SongType.Unit,
+      focusPerformers: undefined
+    },
+    {
+      number: 19,
+      title: "ポピパッパパー",
+      type: SongType.Coupling,
+      focusPerformers: undefined
+    },
+    {
+      number: 20,
+      title: "制服を脱いでサヨナラを…",
+      type: SongType.Unit,
+      focusPerformers: undefined
+    },
+    {
+      number: 21,
+      title: "憂鬱と風船ガム",
+      type: SongType.Unit,
+      focusPerformers: undefined
+    },
+    {
+      number: 22,
+      title: "立ち直り中",
+      type: SongType.Unit,
+      focusPerformers: undefined
+    },
+    {
+      number: 23,
+      title: "失恋したら、顔を洗え!",
+      type: SongType.Unit,
+      focusPerformers: undefined
+    },
+    {
+      number: 24,
+      title: "かき氷の片想い",
+      type: SongType.SecondGeneration,
+      focusPerformers: undefined
+    },
+    {
+      number: 25,
+      title: "大人への近道",
+      type: SongType.Unit,
+      focusPerformers: undefined
+    },
+    {
+      number: 26,
+      title: "君は僕と会わない方がよかったのかな",
+      type: SongType.Under,
+      focusPerformers: undefined
+    },
+    {
+      number: 27,
+      title: "別れ際、もっと好きになる",
+      type: SongType.Under,
+      focusPerformers: undefined
+    },
+    {
+      number: 28,
+      title: "嫉妬の権利",
+      type: SongType.Under,
+      focusPerformers: undefined
+    },
+    {
+      number: 29,
+      title: "不等号",
+      type: SongType.Under,
+      focusPerformers: undefined
+    },
+    {
+      number: 30,
+      title: "環状六号線",
+      type: SongType.Unit,
+      focusPerformers: undefined
+    },
+    {
+      number: 31,
+      title: "口約束",
+      type: SongType.Unit,
+      focusPerformers: undefined
+    },
+    {
+      number: 32,
+      title: "ロマンティックいか焼き",
+      type: SongType.Coupling,
+      focusPerformers: undefined
+    },
+    {
+      number: 33,
+      title: "ハウス!",
+      type: SongType.Coupling,
+      focusPerformers: undefined
+    },
+    {
+      number: 34,
+      title: "そんなバカな…",
+      type: SongType.Coupling,
+      focusPerformers: undefined
+    },
+    {
+      number: 35,
+      title: "シャキイズム",
+      type: SongType.Coupling,
+      focusPerformers: undefined
+    },
+    {
+      number: 36,
+      title: "ロマンスのスタート",
+      type: SongType.Coupling,
+      focusPerformers: undefined
+    },
+  ]
+};
+
+const tomeinairo: IAlbum = {
+  title: "透明な色",
+  number: 1,
+  release: "20165-01-27",
+  shopping: [
+    {
+      title: "iTunes",
+      url: "url"
+    }
+  ],
+  songs: [
+    {
+      number: 1,
+      title: "OVERTURE",
+      type: SongType.Coupling,
+      focusPerformers: undefined
+    },
+    {
+      number: 2,
+      title: "ぐるぐるカーテン",
+      type: SongType.Title,
+      focusPerformers: undefined
+    },
+    {
+      number: 3,
+      title: "おいでシャンプー",
+      type: SongType.Title,
+      focusPerformers: undefined
+    },
+    {
+      number: 4,
+      title: "走れ!Bicycle",
+      type: SongType.Title,
+      focusPerformers: undefined
+    },
+    {
+      number: 5,
+      title: "制服のマネキン",
+      type: SongType.Title,
+      focusPerformers: undefined
+    },
+    {
+      number: 6,
+      title: "君の名は希望",
+      type: SongType.Title,
+      focusPerformers: undefined
+    },
+    {
+      number: 7,
+      title: "ガールズルール",
+      type: SongType.Title,
+      focusPerformers: undefined
+    },
+    {
+      number: 8,
+      title: "バレッタ",
+      type: SongType.Title,
+      focusPerformers: undefined
+    },
+    {
+      number: 9,
+      title: "気づいたら片想い",
+      type: SongType.Title,
+      focusPerformers: undefined
+    },
+    {
+      number: 10,
+      title: "夏のFree&Easy",
+      type: SongType.Title,
+      focusPerformers: undefined
+    },
+    {
+      number: 11,
+      title: "何度目の青空か?",
+      type: SongType.Title,
+      focusPerformers: undefined
+    },
+    {
+      number: 12,
+      title: "誰かは味方",
+      type: SongType.Unit,
+      focusPerformers: undefined
+    },
+    {
+      number: 13,
+      title: "革命の馬",
+      type: SongType.Unit,
+      focusPerformers: undefined
+    },
+    {
+      number: 14,
+      title: "僕がいる場所",
+      type: SongType.Coupling,
+      focusPerformers: undefined
+    },
+    {
+      number: 15,
+      title: "あなたのために弾きたい",
+      type: SongType.Solo,
+      focusPerformers: undefined
+    },
+    {
+      number: 16,
+      title: "他の星から",
+      type: SongType.Unit,
+      focusPerformers: undefined
+    },
+    {
+      number: 17,
+      title: "私のために 誰かのために",
+      type: SongType.Unit,
+      focusPerformers: undefined
+    },
+    {
+      number: 18,
+      title: "せっかちなかたつむり",
+      type: SongType.Unit,
+      focusPerformers: undefined
+    },
+    {
+      number: 19,
+      title: "涙がまだ悲しみだった頃",
+      type: SongType.Under,
+      focusPerformers: undefined
+    },
+    {
+      number: 20,
+      title: "無口なライオン",
+      type: SongType.Unit,
+      focusPerformers: undefined
+    },
+    {
+      number: 21,
+      title: "世界で一番 孤独なLover",
+      type: SongType.Coupling,
+      focusPerformers: undefined
+    },
+    {
+      number: 22,
+      title: "あの日 僕は咄嗟に嘘をついた",
+      type: SongType.Under,
+      focusPerformers: undefined
+    },
+    {
+      number: 23,
+      title: "13日の金曜日",
+      type: SongType.Under,
+      focusPerformers: undefined
+    },
+    {
+      number: 24,
+      title: "失いたくないから",
+      type: SongType.Coupling,
+      focusPerformers: undefined
+    },
+    {
+      number: 25,
+      title: "ダンケシェーン",
+      type: SongType.Unit,
+      focusPerformers: undefined
+    },
+    {
+      number: 26,
+      title: "傾斜する",
+      type: SongType.Coupling,
+      focusPerformers: undefined
+    },
+    {
+      number: 27,
+      title: "なぞの落書き",
+      type: SongType.Unit,
+      focusPerformers: undefined
+    },
+    {
+      number: 28,
+      title: "自由の彼方",
+      type: SongType.Under,
+      focusPerformers: undefined
+    },
+    {
+      number: 29,
+      title: "ひとりよがり",
+      type: SongType.Solo,
       focusPerformers: undefined
     }
   ]
 };
 
 export const albums = {
-  "album-title-1": album1,
-  "album-title-2": album2
+  "僕だけの君〜Under Super Best〜": bokudakenokimi,
+  "生まれてから初めて見た夢": umaretekara,
+  "それぞれの椅子": sorezorenoisu,
+  "透明な色": tomeinairo
 };
